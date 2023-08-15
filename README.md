40ce4f9049982855999c31a42ad0f18e1d696b57
Security Research Team <srt@fox-it.com>
2023-08-15 15:19:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,166 @@
+# Citrix NetScaler Triage
+
+This repository contains a Dissect triage script for Citrix NetScaler devices.
+
+You can use `iocitrix.py` to check for known Indicators of Compromise on a NetScaler Dissect target. It checks for the following things:
+
+* Known strings used in webshells
+* Timestomped files
+* Suspicious cronjobs
+* Unknown SUID binaries
+
+Note that this script is meant to run on acquired disk images of Citrix NetScaler devices and not on the device itself.
+Also see the "Acquiring Citrix NetScaler RAM disk" section about how to create a disk image of the Citrix NetScaler RAM disk.
+
+Ensure that you have the latest version of Dissect, support for Citrix NetScaler was added in this PR: https://github.com/fox-it/dissect.target/pull/357
+
+Disclaimer: While this tool strives for accuracy, it is possible for it to produce false posives or false negatives. Users are advised to cross-check results and use their own judgement before making any decisions based on this tool's output.
+
+## iocitrix.py
+
+Within the `citrix-netscaler-triage` folder, first run `pip install -r requirements.txt` to install the dependencies of this script. You can then run `iocitrix.py <<targets>>` to run an IOC check against one or more forensic images. The script accepts any input that `dissect` can read as a `Target`, such as a `.VMDK`, or a raw disk image. 
+
+```shell
+python3 iocitrix.py image.vmx
+```
+
+If you have also acquired a RAM disk, you can utilize the `MultiRawLoader` of Dissect like such:
+
+```shell
+python3 iocitrix.py md0.img+image.vmdk
+```
+
+Example output:
+```
+(venv) user@dissect:/data/netscaler/image$ python3 iocitrix.py md0.img+da0.img
+<Target md0.img+da0.img>
+
+Disks
+- <RawContainer size=555745286 vs=None>
+- <RawContainer size=21474836486 vs=<DissectVolumeSystem serial=None>>
+
+Volumes
+- <Volume name=None size=555745286 fs=<FfsFilesystem>>
+- <Volume name='part_00000000' size=1717567488 fs=<FfsFilesystem>>
+- <Volume name='part_66600000' size=4401922048 fs=<FfsFilesystem>>
+- <Volume name='part_16cc00000' size=2097152 fs=<FfsFilesystem>>
+- <Volume name='part_16ce00000' size=15353200128 fs=<FfsFilesystem>>
+
+Hostname      : None
+Domain        : None
+IPs           : 10.164.0.39, 10.164.0.10
+OS family     : citrix-netscaler (CitrixBsdPlugin)
+OS version    : NetScaler 13.1 build 30 (ns-13.1-30.52)
+Architecture  : x86_64-citrix-netscaler
+Language(s)   :
+Timezone      : None
+Install date  : 2023-08-08 13:59:38.228043+00:00
+Last activity : 2023-08-11 08:51:13.979536+00:00
+
+
+*** Checking for webshells ***
+
+<ioc/hit type='php-file-permission' alert='Suspicious php permission 0o644' confidence='high' path='/var/netscaler/logon/LogonPoint/uiareas/linux/adminupevents.php'>
+<ioc/hit type='php-file-contents' alert="Suspicious PHP code 'b'array_filter(''" confidence='high' path='/var/netscaler/logon/LogonPoint/uiareas/linux/adminupevents.php'>
+<ioc/hit type='php-file-permission' alert='Suspicious php permission 0o644' confidence='high' path='/var/vpn/config.php'>
+<ioc/hit type='php-file-contents' alert="Suspicious PHP code 'b'array_filter(''" confidence='high' path='/var/vpn/config.php'>
+<ioc/hit type='php-file-permission' alert='Suspicious php permission 0o644' confidence='high' path='/var/vpn/themes/config.php'>
+
+*** Checking for timestomped files ***
+
+
+*** Checking for suspicious cronjobs ***
+
+
+*** Checking for SUID Binaries (this takes a while) ***
+
+<ioc/hit type='binary/suid' alert='Binary with SUID bit set Observed' confidence='medium' path='/tmp/python/bash'>
+
+********************************************************************************
+***                                                                          ***
+*** There were findings for Indicators of Compromise.                        ***
+*** Please consider performing further forensic investigation of the system. ***
+***                                                                          ***
+********************************************************************************
+
+Confidence    Type                 Alert                                       Artefact Location
+------------  -------------------  ------------------------------------------  ---------------------------------------------------------------
+high          php-file-permission  Suspicious php permission 0o644             /var/netscaler/logon/LogonPoint/uiareas/linux/adminupevents.php
+high          php-file-contents    Suspicious PHP code 'b'array_filter(''      /var/netscaler/logon/LogonPoint/uiareas/linux/adminupevents.php
+high          php-file-permission  Suspicious php permission 0o644             /var/vpn/config.php
+high          php-file-contents    Suspicious PHP code 'b'array_filter(''      /var/vpn/config.php
+high          php-file-permission  Suspicious php permission 0o644             /var/vpn/themes/config.php
+medium        binary/suid          Binary with SUID bit set Observed           /tmp/python/bash
+
+All targets analyzed.
+```
+
+## Acquiring Citrix NetScaler RAM disk
+
+Note that the root directory of Citrix NetScaler is a RAM disk, meaning that this is volatile and gone if you shutdown the NetScaler.
+Some volatile configuration changes can be stored on the root disk, such as `crontab`.
+
+When investigating offline on disk images of Citrix NetScalers we recommend to also create a disk image of the RAM disk so that information is not lost.
+You can use the following steps to create a disk image of the RAM disk:
+
+1. Login to Citrix NetScaler over SSH
+2. type in `shell` to start a shell session.
+3. Use the `dd` command to create an image of the RAM disk `/dev/md0`:
+
+```shell
+dd if=/dev/md0 bs=10M of=/var/tmp/md0.img
+```
+4. This takes several seconds as the RAM disk is fairly small, after it is done the disk image is available in `/var/tmp`.
+5. Use `scp` to copy this file over.
+
+Example session:
+
+```
+local $ ssh nsroot@<NETSCALER-IP>
+###############################################################################
+#                                                                             #
+#        WARNING: Access to this system is for authorized users only          #
+#         Disconnect IMMEDIATELY if you are not an authorized user!           #
+#                                                                             #
+###############################################################################
+
+(nsroot@<NETSCALER-IP>) Password: 
+ Done
+> shell
+root@ns# mount
+/dev/md0 on / (ufs, local)
+devfs on /dev (devfs, local, multilabel)
+procfs on /proc (procfs, local)
+/dev/da0s1a on /flash (ufs, local, soft-updates)
+/dev/da0s1e on /var (ufs, local, soft-updates)
+
+root@ns# dd if=/dev/md0 bs=10M of=/var/tmp/md0.img
+53+0 records in
+53+0 records out
+555745280 bytes transferred in 4.753627 secs (116909732 bytes/sec)
+
+root@ns# exit
+logout
+ Done
+> exit
+Bye!
+
+local $ scp -C nsroot@<NETSCALER-IP>:/var/tmp/md0.img md0.img
+md0.img                        100%  530MB   6.6MB/s   01:20     
+```
+
+## Acquiring Citrix NetScaler disks over SSH
+
+If you prefer not to write the disk image to `/var/tmp`, you can also stream the disk image directly over SSH to your own machine without touching the disk of the NetScaler device:
+
+```
+local $ ssh nsroot@<NETSCALER-IP> shell dd if=/dev/md0 bs=10M > md0.img
+```
+
+Do note that the builtin shell of NetScaler outputs a ` Done\n` message before and after your command to stdout, so this needs to be stripped from the file.
+
+This method can also be used to acquire an image of the `/flash` and `/var` disk, which usually resides on `/dev/da0`:
+
+```
+local $ ssh nsroot@<NETSCALER-IP> shell dd if=/dev/da0 bs=10M > da0.img
+```
---
+++ iocitrix.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python3
+#
+# file:   iocitrix.py
+#  
+#  $ python3 iocitrix.py netscaler-image.vmx
+#
+# author: Fox-IT Security Research Team <srt@fox-it.com>
+#
+import argparse
+import os
+import re
+from typing import Iterator
+
+try:
+    from tabulate import tabulate
+except ImportError:
+    raise ImportError("tabulate missing, please use `pip install tabulate`")
+
+try:
+    from dissect.target import Target
+    from dissect.target.tools.info import print_target_info
+    from dissect.util.ts import from_unix
+    from flow.record import RecordDescriptor
+except ImportError:
+    raise ImportError("dissect missing, please use `pip install dissect`")
+
+EXPECTED_PHP_FILE_PERMISSION = 0o444
+
+SUSPICIOUS_PHP_CONTENTS = {b"eval($_", b"base64_decode(", b"http_status_code(", b"http_status_code(", b"array_filter("}
+
+MAXIMUM_BYTE_SIZE_PHP_TO_CHECK_CONTENTS = 2048
+
+# Difference of two weeks between modification time and changed time
+TIMESTOMP_THRESHOLD_SECONDS = (60 * 60) * 24 * 7 * 2
+
+WEBSHELL_PATHS = [
+    "/var/netscaler/logon/",
+    "/var/vpn/",
+    "/var/netscaler/ns_gui/",
+]
+
+
+TIMESTOMP_DIRS = WEBSHELL_PATHS + ["/var/tmp"]
+
+KNOWN_SUID_BINARIES = [
+    "/netscaler/ping",
+    "/netscaler/ping6",
+    "/netscaler/traceroute",
+    "/netscaler/traceroute6",
+    "/sbin/mksnap_ffs",
+    "/sbin/shutdown",
+    "/sbin/poweroff",
+    "/usr/bin/crontab",
+    "/usr/bin/lock",
+    "/usr/bin/login",
+    "/usr/bin/passwd",
+    "/usr/bin/yppasswd",
+    "/usr/bin/su",
+    "/usr/libexec/ssh-keysign",
+    "/bin/umount",
+    "/bin/ping",
+    "/bin/mount",
+    "/bin/su",
+    "/bin/ping6",
+    "/lib64/dbus-1/dbus-daemon-launch-helper",
+    "/usr/bin/atq",
+    "/usr/bin/at",
+    "/usr/bin/sudo",
+    "/usr/bin/newgrp",
+    "/usr/bin/chsh",
+    "/usr/bin/sg",
+    "/usr/bin/gpasswd",
+    "/usr/bin/chfn",
+    "/usr/bin/sudoedit",
+    "/usr/bin/staprun",
+    "/usr/bin/atrm",
+    "/usr/bin/chage",
+    "/usr/libexec/openssh/ssh-keysign",
+    "/usr/sbin/userhelper",
+    "/usr/sbin/usernetctl",
+    "/usr/sbin/ping6",
+    "/opt/likewise/bin/ksu",
+    "/sbin/umount.nfs",
+    "/sbin/pam_timestamp_check",
+    "/sbin/unix_chkpwd",
+    "/sbin/mount.nfs",
+    "/sbin/mount.nfs4",
+    "/sbin/umount.nfs4",
+]
+
+
+EVIL_CRONTAB_CONTENTS = [
+    ("ip address ", re.compile(r"([0-9]{1,3}\.){3}[0-9]{1,3}")),
+    ("/var/tmp", re.compile("/var/tmp")),
+    ("nobody user", re.compile("nobody")),
+]
+
+FindingRecord = RecordDescriptor(
+    "ioc/hit",
+    [
+        ("string", "type"),
+        ("string", "alert"),
+        ("string", "confidence"),
+        ("string", "path"),
+    ],
+)
+
+
+def check_suspicious_php_files(target: Target, start_path) -> Iterator[FindingRecord]:
+    if not target.fs.exists(start_path):
+        return
+    for directory, _, files in target.fs.walk(start_path):
+        for file in files:
+            path = os.path.join(directory, file)
+            if not file.endswith(".php"):
+                continue
+            if (target.fs.stat(path).st_mode & 0o777) != EXPECTED_PHP_FILE_PERMISSION:
+                permission_printable = oct(target.fs.stat(path).st_mode & 0o777)
+                yield FindingRecord(
+                    alert=f"Suspicious php permission {permission_printable}",
+                    confidence="high",
+                    path=path,
+                    type="php-file-permission",
+                )
+
+            if target.fs.stat(path).st_size > MAXIMUM_BYTE_SIZE_PHP_TO_CHECK_CONTENTS:
+                continue
+
+            with target.fs.open(path) as php_file:
+                php_contents = php_file.readlines()
+                for line in php_contents:
+                    for evil in SUSPICIOUS_PHP_CONTENTS:
+                        if evil.lower() in line.lower():
+                            yield FindingRecord(
+                                type="php-file-contents",
+                                path=path,
+                                confidence="high",
+                                alert=f"Suspicious PHP code '{evil}'",
+                            )
+
+
+def check_suid_binaries(target: Target) -> Iterator[tuple[str, str]]:
+    for suid_binary_record in target.suid_binaries():
+        if suid_binary_record.path in KNOWN_SUID_BINARIES:
+            continue
+        yield FindingRecord(
+            type="binary/suid",
+            alert="Binary with SUID bit set Observed",
+            confidence="medium",
+            path=suid_binary_record.path,
+        )
+
+
+def check_crontabs(target: Target):
+    for cronjob_record in target.cronjobs():
+        if cronjob_record._desc.name == "linux/environmentvariable":
+            continue
+        if cronjob_record.user in ["nobody"]:
+            yield FindingRecord(
+                type="cronjob/user",
+                alert="Crontab by nobody user observed",
+                confidence="high",
+                path=cronjob_record.path,
+            )
+        for evil_check in EVIL_CRONTAB_CONTENTS:
+            name, pattern = evil_check
+            if match := pattern.match(cronjob_record.command):
+                yield FindingRecord(
+                    type="cronjob/command",
+                    alert=f"{name} find in crontab comand ({match.group(0)})",
+                    confidence="medium",
+                    path=cronjob_record.path,
+                )
+
+
+def check_timestomps(target: Target):
+    for timestomp_dir in TIMESTOMP_DIRS:
+        for entry in target.fs.path(timestomp_dir).rglob("*"):
+            if not entry.exists():
+                continue
+            stat = entry.lstat()
+            modification_time = from_unix(stat.st_mtime)
+            changed_time = from_unix(stat.st_ctime)
+
+            difference = changed_time - modification_time
+
+            if difference.seconds > TIMESTOMP_THRESHOLD_SECONDS:
+                yield FindingRecord(
+                    type="file/timestomp",
+                    alert=f"Possibly Timestomped File Observed ({difference.seconds} seconds)",
+                    confidence="medium",
+                    path=entry.path,
+                )
+
+
+def ioc_check_target(target: Target) -> list[FindingRecord]:
+    findings = []
+
+    print("\n*** Checking for webshells ***\n")
+    for path in WEBSHELL_PATHS:
+        for finding in check_suspicious_php_files(target, path):
+            print(finding)
+            findings.append(finding)
+
+    print("\n*** Checking for timestomped files ***\n")
+    for finding in check_timestomps(target):
+        print(finding)
+        findings.append(finding)
+
+    print("\n*** Checking for suspicious cronjobs ***\n")
+    for finding in check_crontabs(target):
+        print(finding)
+        findings.append(finding)
+
+    print("\n*** Checking for SUID Binaries (this takes a while) ***\n")
+    for finding in check_suid_binaries(target):
+        print(finding)
+        findings.append(finding)
+
+    return findings
+
+
+def check_targets(target_paths: list[str]) -> None:
+    for path in target_paths:
+        target = Target.open(path)
+        if target.os != "citrix-netscaler":
+            raise ValueError(f"Target not recognized as a citrix-netscaler: {target.path}: {target.os}")
+        print_target_info(target)
+        print("")
+        target_findings = ioc_check_target(target)
+        if len(target_findings) == 0:
+            print("[*] No hits found for IOC checks.")
+        else:
+            print("")
+            print("********************************************************************************")
+            print("***                                                                          ***")
+            print("*** There were findings for Indicators of Compromise.                        ***")
+            print("*** Please consider performing further forensic investigation of the system. ***")
+            print("***                                                                          ***")
+            print("********************************************************************************")
+            print("")
+            # Display in table format
+            table_entries = []
+            for finding in target_findings:
+                table_entries.append(
+                    {
+                        "Confidence": finding.confidence,
+                        "Type": finding.type,
+                        "Alert": finding.alert,
+                        "Artefact Location": finding.path,
+                    }
+                )
+            print(tabulate(table_entries, headers="keys"))
+
+    print("\n\n")
+    print("All targets analyzed.")
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Analyze forensic images of Citrix Netscalers for IOCs")
+    parser.add_argument("targets", metavar="TARGETS", nargs="+", help="Target(s) to load")
+    args = parser.parse_args()
+    check_targets(args.targets)
+
+
+if __name__ == "__main__":
+    main()
