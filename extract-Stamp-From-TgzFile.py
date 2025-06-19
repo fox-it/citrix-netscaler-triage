5104f81e2e7019dccfcad7c3a219043da81e0859
Tijl Deneut <deneut_tijl@hotmail.com>
2025-06-19 16:36:22 +0200

    added extract-Stamp-From-TgzFile.py
    Use this file to generate new timestamps, hashes and versions

---
+++ extract-Stamp-From-TgzFile.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# file:     extract-Stamp-From-TgzFile.py
+# author:   Photubias <github.com/tijldeneut> & yunzheng <github.com/yunzheng>
+# 
+### Open support.citrix.com. Download "Citrix ADC Release" nCore package and save as build-14.1-47.46_nc_64.tgz.
+###   That's all that is needed together with this script
+### The rdx_en.json.gz file is found at build-14.1-47.46_nc_64.tgz\build_zion_47_46_nc_64.tar\ns-14.1-47.46-gui.tar\vpn\js\rdx\core\lang\rdx_en.json.gz
+
+import tarfile, io, datetime, re, sys
+if len(sys.argv)<2: exit('Error: please provide filename as parameter. E.g. extract-Stamp-From-TgzFile.py build-14.1-47.46_nc_64.tgz')
+sFilename = sys.argv[1]
+print(f'Parsing file {sFilename}, might take some time')
+version = re.findall(r'\d+.\d+-\d+.\d+',sFilename)[0]
+tarFile1 = tarfile.open(sFilename,'r:gz')
+
+oNSfile = tarFile1.extractfile(tarFile1.getmember(f'ns-{version}-gui.tar'))
+bNSfile = oNSfile.read()
+tarNSfile = tarfile.open(fileobj=io.BytesIO(bNSfile))
+
+oRDXfile = tarNSfile.extractfile(tarNSfile.getmember('vpn/js/rdx/core/lang/rdx_en.json.gz'))
+bRDXfile = oRDXfile.read()
+
+vhash = ''
+for tinfo in (t for t in tarNSfile if t.name.endswith('index.html')):
+    for line in tarNSfile.extractfile(tinfo):
+        if b'?v=' in line:
+            _, _, vhash = line.partition(b'?v=')
+            vhash, _, _ = vhash.partition(b'"')
+            vhash = vhash.decode()
+
+if bRDXfile.startswith(b'\x1f\x8b\x08\x08') and b'rdx_en.json' in bRDXfile:
+  stamp = int.from_bytes(bRDXfile[4:8], 'little')
+  dt = datetime.datetime.fromtimestamp(stamp, datetime.timezone.utc)
+  print('Extracted timestamp: {},{},{},{}'.format(dt, stamp, vhash, version))
